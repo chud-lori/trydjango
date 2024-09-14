0af92cea7263a9ddf0212f11917fbb08c4253d67
Lori <nur.aziz@finaccel.co>
2024-09-14 23:11:53 +0700

    Add blog module

--- financeapp/financeapp/urls.py
+++ financeapp/financeapp/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+# docstring in file under import
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'financeapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # this is blog
+    url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+# end of line comment
