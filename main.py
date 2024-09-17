2ea7940ffa35d7759f5f5fc918bcdfcb6bb45ce7
Lori <nur.aziz@finaccel.co>
2024-09-17 12:13:10 +0700

    New function on main

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def function_main_lws_01(value):
+    print(f"Value received: {value}")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
